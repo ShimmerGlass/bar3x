059ad0137bdf8773fc3ef67af073ebe25348f751
Mathilde Gilles <ma.gilles@criteo.com>
2020-12-08 12:11:31 +0100

    Show VPN connection names in VPN module

--- ui/module/vpn.go
+++ ui/module/vpn.go
@@ -2,17 +2,22 @@ package module
 
 import (
 	"context"
+	"path/filepath"
+	"strings"
 	"time"
 
 	log "github.com/sirupsen/logrus"
 
 	"github.com/Wifx/gonetworkmanager"
 	"github.com/shimmerglass/bar3x/ui"
+	"github.com/shimmerglass/bar3x/ui/base"
 	"github.com/shimmerglass/bar3x/ui/markup"
 )
 
 type VPN struct {
 	moduleBase
+
+	Txt *base.Text
 
 	mk    *markup.Markup
 	clock *Clock
@@ -29,7 +34,12 @@ func NewVPN(p ui.ParentDrawable, mk *markup.Markup, clock *Clock) *VPN {
 
 func (m *VPN) Init() error {
 	_, err := m.mk.Parse(m, m, `
-		<Icon ref="Root">{icons.lock}</Icon>
+		<Row ref="Root">
+			<Sizer PaddingRight="{h_padding}">
+				<Icon>{icons.lock}</Icon>
+			</Sizer>
+			<Text ref="Txt" />
+		</Row>
 	`)
 	if err != nil {
 		return err
@@ -55,6 +65,9 @@ func (m *VPN) Update(context.Context) {
 		m.SetVisible(false)
 		return
 	}
+
+	activeVPNs := []string{}
+
 	for _, c := range conns {
 		isVPN, err := c.GetPropertyVPN()
 		if err != nil {
@@ -62,10 +75,26 @@ func (m *VPN) Update(context.Context) {
 			continue
 		}
 		if isVPN {
-			m.SetVisible(true)
-			return
+			conn, err := c.GetPropertyConnection()
+			if err != nil {
+				continue
+			}
+			path, err := conn.GetPropertyFilename()
+			if err != nil {
+				continue
+			}
+			file := filepath.Base(path)
+			name := strings.TrimSuffix(file, filepath.Ext(file))
+
+			activeVPNs = append(activeVPNs, name)
 		}
 	}
 
-	m.SetVisible(false)
+	if len(activeVPNs) == 0 {
+		m.SetVisible(false)
+		return
+	}
+
+	m.SetVisible(true)
+	m.Txt.SetText(strings.Join(activeVPNs, ", "))
 }
