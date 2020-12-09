d27c84776e2622171c837904bed1165ff05d7360
Mathilde Gilles <ma.gilles@criteo.com>
2020-12-09 01:40:21 +0100

    Event: fix dispatch

--- bar/event.go
+++ bar/event.go
@@ -11,23 +11,26 @@ func (b *Bar) displatchEvent(ev ui.Event) {
 		h := b.LeftRoot.Height()
 		x, y := b.padding, (b.height-h)/2
 
-		ev.At = ev.At.Sub(image.Pt(x, y))
-		b.LeftRoot.SendEvent(ev)
+		nev := ev
+		nev.At = ev.At.Sub(image.Pt(x, y))
+		b.LeftRoot.SendEvent(nev)
 	}
 
 	{
 		w, h := b.CenterRoot.Width(), b.CenterRoot.Height()
 		x, y := (b.screen.Width-w)/2, (b.height-h)/2
 
-		ev.At = ev.At.Sub(image.Pt(x, y))
-		b.CenterRoot.SendEvent(ev)
+		nev := ev
+		nev.At = ev.At.Sub(image.Pt(x, y))
+		b.CenterRoot.SendEvent(nev)
 	}
 
 	{
 		w, h := b.RightRoot.Width(), b.RightRoot.Height()
 		x, y := b.screen.Width-w-b.padding, (b.height-h)/2
 
-		ev.At = ev.At.Sub(image.Pt(x, y))
-		b.RightRoot.SendEvent(ev)
+		nev := ev
+		nev.At = ev.At.Sub(image.Pt(x, y))
+		b.RightRoot.SendEvent(nev)
 	}
 }
